eb0482143bd9c0bc92bd9c8a4125a0428d6e6a65
mark <mmond@io.com>
2008-11-15 12:03:22 -0600

    app configs

---
+++ eldorado_tuneup/config/environment.rb
@@ -0,0 +1,20 @@
+# Specifies gem version of Rails to use when vendor/rails is not present
+RAILS_GEM_VERSION = '2.1' unless defined? RAILS_GEM_VERSION
+
+require File.join(File.dirname(__FILE__), 'boot')
+
+Rails::Initializer.run do |config|  
+  require 'open-uri'
+  require 'yaml'
+
+  config.time_zone = 'UTC'
+  config.active_record.partial_updates = true
+  config.gem 'fiveruns_tuneup'
+  
+  # The session_key and secret (for verifying session data integrity) are set in config/database.yml
+  db = YAML.load_file('config/database.yml')
+  config.action_controller.session = {
+    :session_key => db[RAILS_ENV]['session_key'],
+    :secret      => db[RAILS_ENV]['secret']
+  }
+end
---
+++ eldorado_tuneup/config/routes.rb
@@ -0,0 +1,40 @@
+ActionController::Routing::Routes.draw do |map|
+  
+  map.root :controller => 'home'
+  
+  map.resources :articles
+  map.resources :avatars, :member => {:select => :post, :deselect => :post}
+  map.resources :categories, :member => {:confirm_delete => :get}
+  map.resources :comments
+  map.resources :events
+  map.resources :forums, :member => {:confirm_delete => :get}
+  map.resources :headers, :member => {:vote_up => :post, :vote_down => :post}
+  map.resources :messages, :collection => {:more => :get, :refresh => :get}
+  map.resources :posts, :member => {:quote => :get, :topic => :get}
+  map.resources :ranks
+  map.resources :settings
+  map.resources :subscriptions
+  map.resources :themes, :member => {:select => :post, :deselect => :post}
+  map.resources :topics, :member => {:show_new => :get}, :collection => {:mark_all_viewed => :get}
+  map.resources :uploads
+  map.resources :users, :member => {:admin => :post, :ban => :get, :remove_ban => :post, :confirm_delete => :get}, 
+                        :has_many => [:articles, :posts]
+  
+  map.search 'search', :controller => 'search', :action => 'index'
+  map.refresh_chatters 'refresh_chatters', :controller => 'messages', :action => 'refresh_chatters'
+  
+  map.login 'login', :controller => 'users', :action => 'login'
+  map.logout 'logout', :controller => 'users', :action => 'logout'
+  map.register 'register', :controller => 'users', :action => 'new'
+  
+  map.admin 'admin', :controller => 'settings', :action => 'index'
+  map.blog 'blog', :controller => 'articles', :action => 'index'
+  map.blog_archives 'blog/archives', :controller => 'articles', :action => 'archives'
+  map.chat 'chat', :controller => 'messages', :action => 'index'
+  map.files 'files', :controller => 'uploads', :action => 'index'
+  map.forum_root 'forum', :controller => 'forums', :action => 'index'
+  
+  map.exceptions 'logged_exceptions/:action/:id', :controller => 'logged_exceptions', :action => 'index', :id => nil
+  
+  map.catch_all '*path', :controller => 'topics', :action => 'unknown_request'
+end
